48b2bca4dc974e23b515a0551dc6cb93117ac86d
alexpic911 <145979020+alexpic911@users.noreply.github.com>
2024-02-21 06:17:56 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+"""
+A sample Hello World server.
+"""
+import os
+from flask import Flask, render_template
+import topicwizard
+import joblib
+# pylint: disable=C0103
+topic_data = joblib.load('/Users/alessandro.piccolo/PycharmProjects/pythonProject1/topic_data.joblib')
+
+
+app_test = topicwizard.get_dash_app(topic_data,exclude_pages = {'words','documents'})
+
+
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app_test.run(debug=False, port=8080)
